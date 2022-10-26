ced5e868e6ba14ceae68f62b781bb6844b9daa89
Raad <ashabulraad@gmail.com>
2022-10-27 04:40:43 +0600

    Navbar commit

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import logo from '../../../assets/Images/logo.png';
 
 const Header = () => {
     return (
-        <div className='header'>
-            <h2>Header connected</h2>
+        <div className='header px-40 py-5'>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <button className="btn btn-ghost normal-case text-4xl">
+                        <img className='w-10 rounded mr-3' src={logo} alt="Logo" />AlgoWizR
+                    </button>
+                </div>
+                <div className='btn-navigation'>
+                    <button className="btn btn-outline btn-sm mx-2 btn-accent">COURSES</button>
+                    <button className="btn btn-outline btn-sm mx-2 btn-accent">FAQ</button>
+                    <button className="btn btn-outline btn-sm mx-2  btn-accent">BLOGS</button>
+                </div>
+                <div className="flex-none ml-5">
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="https://placeimg.com/80/80/people" alt="User" />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><button>Profile</button></li>
+                            <div className="form-control">
+                                <label className="label cursor-pointer">
+                                    <span className="label-text">Dark Mode</span>
+                                    <input type="checkbox" className="toggle toggle-accent" checked />
+                                </label>
+                            </div>
+                            <li><button>Logout</button></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
