857096e3ed80dd2125f4938fafdfaf3d88ea8ade
Raad <ashabulraad@gmail.com>
2022-10-26 15:18:27 +0600

    Components commit

---
+++ src/Components/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2>Header connected</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Shared/RightSide/RightSide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSide = () => {
+    return (
+        <div className='right-side'>
+            <h2>Right side connected</h2>
+        </div>
+    );
+};
+
+export default RightSide;
