8ca93d939547d31e886e2034671c56b69048e83e
Raad <ashabulraad@gmail.com>
2022-10-27 14:58:07 +0600

    Credentials fetch

--- src/Components/Authentication/Login/Login.js
+++ src/Components/Authentication/Login/Login.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const Login = () => {
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+
     return (
         <div className='login-form'>
             <div className="hero min-h-screen bg-base-200">
@@ -10,26 +18,28 @@ const Login = () => {
                         <p className="py-6">Login for free and get started with your programming journey.</p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" placeholder="Email Address" className="input input-bordered" required />
+                        <form onSubmit={handleLogin}>
+                            <div className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" name='email' placeholder="Email Address" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" name='password' placeholder="Password" className="input input-bordered" required />
+                                    <label className="label">
+                                        <button className="label-text-alt link link-hover">Don't have an account?</button>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-accent">Login</button>
+                                </div>
                             </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" placeholder="Password" className="input input-bordered" required />
-                                <label className="label">
-                                    <button className="label-text-alt link link-hover">Don't have an account?</button>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-accent">Login</button>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/Components/Authentication/SignUp/SignUp.js
+++ src/Components/Authentication/SignUp/SignUp.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const SignUp = () => {
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+    }
+
     return (
         <div className='signup-form'>
             <div className="hero min-h-screen bg-base-200">
@@ -10,32 +19,34 @@ const SignUp = () => {
                         <p className="py-6">Sign up for free and get started with your programming journey.</p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Name</span>
-                                </label>
-                                <input type="text" placeholder="Full name" className="input input-bordered" required />
+                        <form onSubmit={handleSignUp}>
+                            <div className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="text" name='name' placeholder="Full name" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" name='email' placeholder="Email Address" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" name='password' placeholder="Password" className="input input-bordered" required />
+                                    <label className="label">
+                                        <button className="label-text-alt link link-hover">Already have an account?</button>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-accent">Login</button>
+                                </div>
                             </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" placeholder="Email Address" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" placeholder="Password" className="input input-bordered" required />
-                                <label className="label">
-                                    <button className="label-text-alt link link-hover">Already have an account?</button>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-accent">Login</button>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
