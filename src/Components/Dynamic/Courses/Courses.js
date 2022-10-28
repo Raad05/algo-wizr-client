656a9568108173d60f30523123409935cb62c63e
Raad <ashabulraad@gmail.com>
2022-10-28 18:43:11 +0600

    Course fetch

--- src/Components/Dynamic/Courses/Courses.js
+++ src/Components/Dynamic/Courses/Courses.js
@@ -1,69 +1,32 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/courses')
+            .then(res => res.json())
+            .then(data => setCourses(data));
+    }, [])
+
     return (
         <div className='courses px-5 py-10 mr-10'>
+            <h1 className='text-xl my-5 font-bold'>Total Courses: {courses.length}</h1>
             <div className='grid grid-cols-3 gap-20'>
-                <div className="card w-80 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-accent">Buy Now</button>
+                {
+                    courses.map(course => <div key={course._id} className="card w-80 bg-base-100 shadow-xl mt-10">
+                        <figure><img src={course.thumbnail} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{course.name}</h2>
+                            <p>{course.details}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-accent">ENROLL</button>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="card w-80 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-accent">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-80 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-accent">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-80 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-accent">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-80 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-accent">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-80 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-accent">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
+                    </div>)
+                }
             </div>
         </div>
     );
