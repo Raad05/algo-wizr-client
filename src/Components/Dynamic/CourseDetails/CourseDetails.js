4e0a93b50deb4de9b272257e6e7d32d200dc6de5
Raad <ashabulraad@gmail.com>
2022-11-01 20:12:57 +0600

    Details update

--- src/Components/Dynamic/CourseDetails/CourseDetails.js
+++ src/Components/Dynamic/CourseDetails/CourseDetails.js
@@ -4,16 +4,17 @@ import Header from '../../Shared/Header/Header';
 
 const CourseDetails = () => {
     const courseDetails = useLoaderData();
+    const { thumbnail, name, details, price, enrolled } = courseDetails
 
     return (
         <div className='course-details'>
             <Header></Header>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col">
-                    <img src={courseDetails.thumbnail} className="max-w-sm rounded-lg shadow-2xl" alt='course-img' />
+                    <img src={thumbnail} className="max-w-sm rounded-lg shadow-2xl" alt='course-img' />
                     <div>
-                        <h1 className="text-5xl font-bold">{courseDetails.name}</h1>
-                        <p className="py-6 pr-10">{courseDetails.details}</p>
+                        <h1 className="text-5xl font-bold">{name}</h1>
+                        <p className="py-6 pr-10">{details}</p>
                         <button className="btn btn-accent">GET PREMIUM ACCESS</button>
                     </div>
                 </div>
