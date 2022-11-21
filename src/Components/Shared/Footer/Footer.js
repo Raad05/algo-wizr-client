28d601b7461e53361ff2a8d70b75bf7155322f4c
Yamin Raad <yamin.raad6109@gmail.com>
2022-11-22 00:14:02 +0600

    Footer

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer mt-10'>
-
+            <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+                <div>
+                    <p>Copyright © 2022 - All rights reserved by AlgoWizR Ltd.</p>
+                </div>
+            </footer>
         </div>
     );
 };
